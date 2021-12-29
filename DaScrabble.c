e328cffe613548143c7206b81665462546c6959d
dafanf <dafanurulfauziansyah97@gmail.com>
2021-12-29 16:07:40 +0700

    adding struct datatype

--- DaScrabble.c
+++ DaScrabble.c
@@ -1,8 +1,31 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdbool.h>
+
+typedef struct {
+	char huruf;
+	int nilaiHuruf;
+	int jumlah;
+} DataHuruf;
+
+typedef struct {
+	DataHuruf isiHuruf;
+	bool isLetter2;
+	bool isLetter3;
+	bool isWord2;
+	bool isWord3;
+} IsiPapan;
+
+typedef struct{
+   char nama_pemain[30];
+   int score;
+   char huruf[7];
+   bool iskomputer;
+}DataPemain;
 
 int main(){
-	
+	IsiPapan Papan;
+	DataPemain Pemain;
 	printf("DaScrabble");
 	
 	return 0;
